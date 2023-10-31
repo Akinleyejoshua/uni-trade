6138fed78bbce0b93c6c59da526849fddd67e981
Joshua <akinleyejoshua.dev@gmail.com>
2023-10-31 18:48:02 +0100

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export const request = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://uni-trade-api-production.up.railway.app",
     headers: {
         Accept: 'application/json,text/plain,*/*',
         'Content-Type': 'application/json',
